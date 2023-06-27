8927737c49216247ab09339192829769f21e65c0
VENOM-InstantDeath <mohathepro111@gmail.com>
2023-06-27 01:34:47 -0300

    Added Control sequences for left and right arrows and 'w'

--- improved_input.c
+++ improved_input.c
@@ -11,6 +11,32 @@ void cbreak(struct termios *tty) {
 	tcsetattr(0, TCSADRAIN, tty);
 }
 
+void backspace(struct string* str, int *p) {
+	if (!(*p)) return;;
+	string_popat(str, (*p)-1);
+	(*p)--;
+	printf("\033[1D\033[s\033[0K");
+	for (int i=*p; i<str->size; i++) {printf("%c", str->str[i]);}
+	printf("\033[u");
+	fflush(stdout);
+}
+
+void right(struct string *str, int *p) {
+	if ((*p) < str->size) {
+		printf("\033[1C");
+		fflush(stdout);
+		(*p)++;
+	}
+}
+
+void left(struct string *str, int* p) {
+	if (p) {
+		printf("\033[1D");
+		fflush(stdout);
+		(*p)--;
+	}
+}
+
 int handleEsc(struct pollfd *rdfd, struct string *str, int *p) {
 	if (!poll(rdfd, 1, 5)) {
 		string_free(str);
@@ -24,18 +50,21 @@ int handleEsc(struct pollfd *rdfd, struct string *str, int *p) {
 		read(0, ch, 1);
 		key[keyp] = ch[0]; keyp++;
 	}
-	if (key[0]==91 && key[1]==67) {
-		if ((*p) < str->size) {
-			printf("\033[1C");
-			fflush(stdout);
-			(*p)++;
-		}
-	}
-	else if (key[0]==91 && key[1]==68) {
-		if (p) {
-			printf("\033[1D");
-			fflush(stdout);
-			(*p)--;
+	if (key[0]==91) {
+		if (key[1]==67) right(str, p);
+		else if (key[1]==68) left(str, p);
+		else if (key[1]==49) {
+			char ch[3];
+			read(0, ch, 3);
+			if (ch[2] == 67) {
+				while (*p != str->size && str->str[(*p)+1] != ' ') right(str, p);
+				while (*p != str->size && str->str[(*p)+1] == ' ') right(str, p);
+				if (*p != str->size) right(str, p);
+			}
+			else if (ch[2] == 68) {
+				while (*p && str->str[(*p)-1] == ' ') left(str, p);
+				while (*p && str->str[(*p)-1] != ' ') left(str, p);
+			}
 		}
 	}
 	return 1;
@@ -61,30 +90,29 @@ int improved_input(struct string *str) {
 			}
 			continue;
 		}
-		if (ch[0] == 4) {  /*Ctrl+D*/
+		else if (ch[0] == 4) {  /*Ctrl+D*/
 			string_free(str);
 			str->str = NULL;
 			printf("\n");
 			tcsetattr(0, TCSADRAIN, &old);
 			return 0;
 		}
-		if (ch[0] == 10) {  /*Enter*/
+		else if (ch[0] == 10) {  /*Enter*/
 			printf("\n");
 			printf("%d\n", str->size);
 			string_addch(str, 0);
 			tcsetattr(0, TCSADRAIN, &old);
 			return 1;
 		}
-		if (ch[0] == 127) {  /*Backspace*/
-			printf("\033[1D \033[1D");
-			fflush(stdout);
-			if (string_popat(str, p-1)) {
-				string_free(str);
-				str->str = NULL;
-				return 0;
-			}
-			p--;
+		else if (ch[0] == 127 || ch[0] == 8) {  /*Backspace*/
+			backspace(str, &p);
 			continue;
+		}
+		else if (ch[0] == 23) {  /*Ctrl+W*/
+			while (p && str->str[(p)-1] == ' ') backspace(str, &p);
+			while (p && str->str[(p)-1] != ' ') backspace(str, &p);
+			continue;
+
 		}
 		string_addchat(str, ch[0], p);
 		printf("\033[s\033[0K");
